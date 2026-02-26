5988c4de6cab911e3df5db4adcf171039f17f6a6
RAMBOhatagais <ramboivanilic@gmail.com>
2026-02-26 20:14:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ def home():
     return "Bot alive"
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
 
 def keep_alive():
     t = Thread(target=run)
